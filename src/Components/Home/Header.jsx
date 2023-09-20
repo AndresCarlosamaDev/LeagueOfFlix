c420d22caa14cbfa2ef35279d4ba0c7e2adea10d
AndresCarlosamaDev <andrescarlosdev@gmail.com>
2023-09-19 21:32:41 -0500

    Json-server-vercel 12

--- src/Components/Home/Header.jsx
+++ src/Components/Home/Header.jsx
@@ -29,7 +29,7 @@ export const Header = () => {
           </a>
         </div>
         {
-          displayBtn() && <a href="videos"><Button contextData={contextData.btnHeader}/></a>
+          displayBtn() && <a href="https://league-of-flix.vercel.app/videos"><Button contextData={contextData.btnHeader}/></a>
         }
           
       </div>

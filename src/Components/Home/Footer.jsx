680cd8f3dc8f6531eaac85143c3305bd0b3ab78f
AndresCarlosamaDev <andrescarlosdev@gmail.com>
2023-09-21 23:38:51 -0500

    Responsive Test Home Page

--- src/Components/Home/Footer.jsx
+++ src/Components/Home/Footer.jsx
@@ -1,6 +1,10 @@
 import React from "react";
 import '../../assets/css/footer.css'
 import logo from '../../assets/img/Logo.png'
+//Icons
+import GitHubIcon from '@mui/icons-material/GitHub';
+import LinkedInIcon from '@mui/icons-material/LinkedIn';
+import LanguageIcon from '@mui/icons-material/Language';
 
 
 export const Footer = () => {
@@ -12,6 +16,11 @@ export const Footer = () => {
                 </div>
                 <div className="footer_h4">
                     <h4>Desarrollado por Andrés Carlosama</h4>
+                </div>
+                <div className="footer_links">
+                    <a href="https://github.com/AndresCarlosamaDev"><GitHubIcon/></a>
+                    <a href="https://www.linkedin.com/in/andr%C3%A9scarlosama/"><LinkedInIcon/></a>
+                    <a href="/"><LanguageIcon/></a>
                 </div>
             </div>
         </footer>
